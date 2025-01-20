e6842a254fb2c5b3dfec849692665be8d7897b63
Timo Weiss <timo.weiss@protonmail.ch>
2025-01-20 16:37:08 +0100

    added plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,28 @@
-import "@/styles/globals.css"
-import { Inter } from "next/font/google"
-import type { Metadata } from "next"
+import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Blob Generator",
-  description: "Generate beautiful gradient blob backgrounds for your website",
-}
+    title: 'Blob Generator',
+    description:
+        'Generate beautiful gradient blob backgrounds for your website',
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+            <script
+                defer
+                data-domain="blurry-blob-background.timoweiss.me"
+                src="https://plausible.io/js/script.js"
+            ></script>
+        </html>
+    );
 }
-
