7fc78673909b12363d83adbbf607755034fefa89
Timo Weiss <timo.weiss@protonmail.ch>
2025-01-21 14:35:53 +0100

    SEO improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,27 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
     title: 'Animated Blurry Blob Background Generator',
     description: 'Generate animated blurry blob backgrounds',
+    openGraph: {
+        title: 'Animated Blurry Blob Background Generator',
+        description: 'Generate animated blurry blob backgrounds',
+        url: 'https://blurry-blob-background.timoweiss.me',
+        type: 'website',
+        images: [
+            {
+                url: 'https://blurry-blob-background.timoweiss.me/og-image.jpg',
+                width: 1200,
+                height: 600,
+                alt: 'Animated Blurry Blob Background Generator',
+            },
+        ],
+    },
+    twitter: {
+        title: 'Animated Blurry Blob Background Generator',
+        description: 'Generate animated blurry blob backgrounds',
+        images: ['https://blurry-blob-background.timoweiss.me/og-image.jpg'],
+        card: 'summary_large_image',
+        creator: '@timooweiss',
+    },
 };
 
 export default function RootLayout({
